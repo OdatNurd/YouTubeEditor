47e14603b5cb9a3d28ea7a2389c0678b104a70a5
Terence Martin <github@nurdz.com>
2020-12-21 21:34:08 -0800

    Factor video browsing code into the core code
    The code for prompting for a video is going to be used in at least two
    places, so it has been hoisted in this commit and refactored to be a
    new common selection routine similar to the previous YouTubeRequest
    class.
    
    In this flow, the only thing that need be implemented to support the
    browse is the method that gets told when a video is selected (which by
    default prompts for timecode) and when a video toc entry is chosen.

--- src/commands/get_video_link.py
+++ src/commands/get_video_link.py
@@ -1,145 +1,25 @@
 import sublime
 import sublime_plugin
 
-from sublime import QuickPanelItem
 
-from ..core import YoutubeRequest
-from ...lib import log, dotty, make_video_link, copy_video_link, select_video
-from ...lib import select_playlist, select_tag, select_timecode
+from ..core import YouTubeVideoSelect
+from ...lib import make_video_link, copy_video_link
 
 
 ###----------------------------------------------------------------------------
 
 
-# The uploads playlist doesn't appear in the list of playlists associated with
-# a user because it's channel specific and not user specific. This is a sample
-# dotty entry with just enough information to allow for populating that
-# playlist into a chooser.
-#
-# The actual ID of the placeholder needs to be established at the point where
-# the data is actually collected.
-_upload_template = {
-    "id": "placeholder",
-    "snippet": {
-        "title": "Uploaded Videos"
-    },
-    "status": {
-        "privacyStatus": "private",
-    },
-    "contentDetails": {
-        # We don't know how many items are in the uploads playlist until we
-        # fetch the contents of it. The display code in the chooser will use
-        # markup to tell the user the list size is unknown in this case.
-        # "itemCount": 0
-    }
-
-}
-
-
-###----------------------------------------------------------------------------
-
-
-class YoutubeEditorGetVideoLinkCommand(YoutubeRequest, sublime_plugin.ApplicationCommand):
+class YoutubeEditorGetVideoLinkCommand(YouTubeVideoSelect, sublime_plugin.ApplicationCommand):
     """
-    Generate a list of all videos for the user's YouTube channel and display
-    them in a quick panel. Choosing a video from the list will copy the URL
-    to the view page for that video into the clipboard.
+    Prompt the user to select a timecode in a video, and then copy the link to
+    that timecode (or the video in general) to the clipboard.
 
-    This command uses previously cached credentials if there are any, and
-    requests the user to log in first if not.
+    The arguments by_tags and by_playlists control how the browse works; if
+    provided, each one adds an extra layer of lookup to help drill down and
+    find the desired video.
     """
-    def _authorized(self, request, result):
-        self.use_tags = self.run_args.get("by_tags", False)
-        self.use_playlists = self.run_args.get("by_playlists", False)
-        self.request("channel_list", reason="Get Channel Info")
-
-    def _channel_list(self, request, result):
-        self.channel = result[0]
-
-        # Make a fake playlist from a template; populate it with the public
-        # video count. The count will be adjusted later if/when the user
-        # browses into the Uploads playlist.
-        self.uploads_playlist = dotty.dotty(_upload_template)
-        self.uploads_playlist['contentDetails.itemCount'] = self.channel['statistics.videoCount']
-        self.uploads_playlist['id'] = self.channel['contentDetails.relatedPlaylists.uploads']
-
-        if self.use_playlists:
-            self.request("playlist_list", channel_id=self.channel['id'],
-                         reason="Get user playlists")
-        else:
-            self.pick_playlist(self.uploads_playlist)
-
-    def _playlist_list(self, request, result):
-        self.playlists = sorted(result, key=lambda k: k["snippet.title"])
-        self.playlists.insert(0, self.uploads_playlist)
-
-        select_playlist(self.playlists, self.pick_playlist)
-
-    def _playlist_contents(self, request, result):
-        if self.use_tags:
-            select_tag(result, self.pick_tag, show_back=self.use_playlists, placeholder="Copy video link from tag")
-        else:
-            # If this is the uploads playlist, update the video count to
-            # include non-public videos.
-            if request["playlist_id"] == self.uploads_playlist['id']:
-                self.uploads_playlist['contentDetails.itemCount'] = len(result)
-
-            # Pass the video list as the tag_list to the lambda so it can be
-            # picked up and used again if the user goes back while editing the
-            # timecode.
-            videos = sorted(result, key=lambda k: int(k["statistics.viewCount"]), reverse=True)
-            select_video(videos, lambda vid: self.select_video(vid, None, videos),
-                         show_back=self.use_playlists,
-                         placeholder="Copy video link")
-
-    def pick_playlist(self, playlist):
-        if playlist != None:
-            self.request("playlist_contents",
-                          reason="Get playlist contents",
-                          playlist_id=playlist['id'])
-
-    def pick_tag(self, tag, tag_list):
-        if tag is not None:
-            if tag == "_back":
-                if self.use_playlists:
-                    return select_playlist(self.playlists, self.pick_playlist)
-
-            videos = sorted(tag_list[tag], key=lambda k: int(k["statistics.viewCount"]), reverse=True)
-
-            placeholder = "Copy video link from tag '%s'" % tag
-            # Video ID is in contentDetails.videoId for short results or id for
-            # full details (due to it being a different type of request)
-            select_video(videos, lambda vid: self.select_video(vid, tag, tag_list),
-                         show_back=True, placeholder=placeholder)
-
-    def select_video(self, video, tag, tag_list):
-        if video is None:
-            return
-
-        if video['id'] == "_back":
-            # When using both tags and playlists, the browse order should send
-            # us back to tags first and from there to playlists.
-            if self.use_tags:
-                return select_tag(None, self.pick_tag, self.use_playlists, tag_list)
-
-            return select_playlist(self.playlists, self.pick_playlist)
-
-
-        select_timecode(video, lambda a, b: self.pick_toc(a, b, video, tag, tag_list),
-                        show_back=True)
-
-    def pick_toc(self, timecode, text, video, tag, tag_list):
-        if timecode != None:
-            if timecode == "_back":
-                if self.use_tags:
-                    return self.pick_tag(tag, tag_list)
-                else:
-                    return select_video(tag_list, lambda vid: self.select_video(vid, None, None),
-                                        show_back=self.use_playlists,
-                                        placeholder="Copy video link")
-
-            copy_video_link(make_video_link(video['id'], timecode),
-                            video['snippet.title'])
+    def picked_toc(self, timecode, text, video):
+        copy_video_link(make_video_link(video['id'], timecode), video['snippet.title'])
 
 
 ###----------------------------------------------------------------------------
--- src/core.py
+++ src/core.py
@@ -3,7 +3,8 @@ import sublime_plugin
 
 import os
 
-from ..lib import log, setup_log_panel, yte_setting
+from ..lib import log, setup_log_panel, yte_setting, dotty
+from ..lib import select_video, select_playlist, select_tag, select_timecode
 from ..lib import Request, NetworkManager, stored_credentials_path
 
 # TODO:
@@ -27,6 +28,33 @@ from ..lib import Request, NetworkManager, stored_credentials_path
 # Our global network manager object
 netManager = None
 
+
+###----------------------------------------------------------------------------
+
+
+# The uploads playlist doesn't appear in the list of playlists associated with
+# a user because it's channel specific and not user specific. This is a sample
+# dotty entry with just enough information to allow for populating that
+# playlist into a chooser.
+#
+# The actual ID of the placeholder needs to be established at the point where
+# the data is actually collected.
+_upload_template = {
+    "id": "placeholder",
+    "snippet": {
+        "title": "Uploaded Videos"
+    },
+    "status": {
+        "privacyStatus": "private",
+    },
+    "contentDetails": {
+        # We don't know how many items are in the uploads playlist until we
+        # fetch the contents of it. The display code in the chooser will use
+        # markup to tell the user the list size is unknown in this case.
+        # "itemCount": 0
+    }
+
+}
 
 ###----------------------------------------------------------------------------
 
@@ -151,4 +179,136 @@ class YoutubeRequest():
         return youtube_has_credentials()
 
 
-## ----------------------------------------------------------------------------
+###----------------------------------------------------------------------------
+
+
+class YouTubeVideoSelect(YoutubeRequest):
+    """
+    This class is a specialization on YoutubeRequest that specifically presumes
+    that the ultimate goal is to have the user select a video for some purpose.
+
+    The sequence of items here is:
+        - Gather channel information
+        - Gather list of playlists and prompt (or; assume uploads playlist)
+        - Gather contents of selected playlist
+        - Prompt by tags on videos in the playlist (optional based on args)
+        - Prompt for a video (either in the tags or in the playlist)
+        - Prompt for a timecode in the video (if any)
+
+    """
+    def _authorized(self, request, result):
+        self.use_tags = self.run_args.get("by_tags", False)
+        self.use_playlists = self.run_args.get("by_playlists", False)
+        self.request("channel_list", reason="Get Channel Info")
+
+    def _channel_list(self, request, result):
+        self.channel = result[0]
+
+        # Make a fake playlist from a template; populate it with the public
+        # video count. The count will be adjusted later if/when the user
+        # browses into the Uploads playlist.
+        self.uploads_playlist = dotty.dotty(_upload_template)
+        self.uploads_playlist['contentDetails.itemCount'] = self.channel['statistics.videoCount']
+        self.uploads_playlist['id'] = self.channel['contentDetails.relatedPlaylists.uploads']
+
+        if self.use_playlists:
+            self.request("playlist_list", channel_id=self.channel['id'],
+                         reason="Get user playlists")
+        else:
+            self.pick_playlist(self.uploads_playlist)
+
+    def _playlist_list(self, request, result):
+        self.playlists = sorted(result, key=lambda k: k["snippet.title"])
+        self.playlists.insert(0, self.uploads_playlist)
+
+        select_playlist(self.playlists, self.pick_playlist)
+
+    def _playlist_contents(self, request, result):
+        if self.use_tags:
+            select_tag(result, self.pick_tag, show_back=self.use_playlists, placeholder="Copy video link from tag")
+        else:
+            # If this is the uploads playlist, update the video count to
+            # include non-public videos.
+            if request["playlist_id"] == self.uploads_playlist['id']:
+                self.uploads_playlist['contentDetails.itemCount'] = len(result)
+
+            # Pass the video list as the tag_list to the lambda so it can be
+            # picked up and used again if the user goes back while editing the
+            # timecode.
+            videos = sorted(result, key=lambda k: int(k["statistics.viewCount"]), reverse=True)
+            select_video(videos, lambda vid: self.select_video(vid, None, videos),
+                         show_back=self.use_playlists,
+                         placeholder="Copy video link")
+
+    def pick_playlist(self, playlist):
+        if playlist != None:
+            self.request("playlist_contents",
+                          reason="Get playlist contents",
+                          playlist_id=playlist['id'])
+
+    def pick_tag(self, tag, tag_list):
+        if tag is not None:
+            if tag == "_back":
+                if self.use_playlists:
+                    return select_playlist(self.playlists, self.pick_playlist)
+
+            videos = sorted(tag_list[tag], key=lambda k: int(k["statistics.viewCount"]), reverse=True)
+
+            placeholder = "Copy video link from tag '%s'" % tag
+            # Video ID is in contentDetails.videoId for short results or id for
+            # full details (due to it being a different type of request)
+            select_video(videos, lambda vid: self.select_video(vid, tag, tag_list),
+                         show_back=True, placeholder=placeholder)
+
+    def select_video(self, video, tag, tag_list):
+        if video is None:
+            return
+
+        if video['id'] == "_back":
+            # When using both tags and playlists, the browse order should send
+            # us back to tags first and from there to playlists.
+            if self.use_tags:
+                return select_tag(None, self.pick_tag, self.use_playlists, tag_list)
+
+            return select_playlist(self.playlists, self.pick_playlist)
+
+        self.picked_video(video, tag, tag_list)
+
+
+    def pick_toc(self, timecode, text, video, tag, tag_list):
+        if timecode != None:
+            if timecode == "_back":
+                if self.use_tags:
+                    return self.pick_tag(tag, tag_list)
+                else:
+                    return select_video(tag_list, lambda vid: self.select_video(vid, None, None),
+                                        show_back=self.use_playlists,
+                                        placeholder="Copy video link")
+
+            self.picked_toc(timecode, text, video)
+
+    def picked_video(self, video, tag, tag_list):
+        """
+        Override this if you want to know what video the user selected; the
+        default will continue on to prompt the user for a timecode contained
+        in the video instead.
+
+        video represents the video chosen by the user, and tag is the tag they
+        chose (if prompted; otherwise it is None). The tag_list argument should
+        be ignored by outside code, as its value and use changes depending on
+        how the user is browsing around in the content.
+        """
+        select_timecode(video, lambda a, b: self.pick_toc(a, b, video, tag, tag_list),
+                        show_back=True)
+
+    def picked_toc(self, timecode, text, video):
+        """
+        Override this if you want to know what timecode the user selected from
+        the table of contents of their selected video. You get told the
+        timecode string, the text of the TOC entry associated with it, and the
+        information on the video the user selected.
+        """
+        pass
+
+
+###----------------------------------------------------------------------------
